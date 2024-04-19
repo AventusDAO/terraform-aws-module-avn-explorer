1bafbcbf0b3ebddf5b66989cd10845197353e53c
Vucomir Ianculov <vukomir@ianculov.ro>
2024-04-19 16:37:35 +0300

    fix undefined variable

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ module "monitoring" {
   source = "git@github.com:Aventus-Network-Services/terraform-aws-rds-monitoring?ref=v0.1.0"
 
   sns_topic         = var.monitoring_sns_topic
-  alarm_name_prefix = "${title(local.env)}-${each.key}"
+  alarm_name_prefix = "${title(var.environment)}-${each.key}"
   db_instance_id    = each.key
   tags              = var.tags
 
