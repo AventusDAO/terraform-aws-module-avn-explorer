971f94ecc4d57ce4d0f6e425904dfe15e471c7ca
stoyanyanevft <162441098+stoyanyanevft@users.noreply.github.com>
2025-03-20 11:17:28 +0200

    testing lifecycle

--- main.tf
+++ main.tf
@@ -94,6 +94,12 @@ module "eks_iam_role" {
     for k, v in local.explorer_components : k => v
     if v.enabled
   }
+
+    lifecycle {
+    ignore_changes = [
+      assume_role_policy
+    ]
+  }
 }
 
 resource "aws_secretsmanager_secret" "this" {
