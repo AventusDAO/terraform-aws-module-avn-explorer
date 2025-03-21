a3c82378eb8195eedbe03409c4be95adad7f10df
Vucomir Ianculov <vukomir@ianculov.ro>
2025-03-21 11:50:46 +0200

    fix: terraform recouse reapply

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.31.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.0"
+      version = ">= 3.7.1"
     }
   }
 }
